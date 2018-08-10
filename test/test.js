c414ea011c4597116f8f6e887d5ae5d47e340ae3
casyplace <casy@casy-place.com>
2018-08-10 13:53:51 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 'use strict';
 
 var request = require("supertest"),
-	app = require("../app").getApp;
+	app = require("/app").getApp;
 
 describe('GET /', function(){
   it('expexts HTTP response 200', function(done){
